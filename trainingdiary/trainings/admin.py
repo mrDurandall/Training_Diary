733a9452888b8f5f3216769b96239a98a263828c
Turganov Ivan <mrdurandall@gmail.com>
2022-07-31 21:00:55 +0300

    set model registered in admin

--- trainingdiary/trainings/admin.py
+++ trainingdiary/trainings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Training
+from .models import Training, ExerciseSet
 
 
-admin.site.register(Training)
+admin.site.register(Training,
+                    ExerciseSet)
